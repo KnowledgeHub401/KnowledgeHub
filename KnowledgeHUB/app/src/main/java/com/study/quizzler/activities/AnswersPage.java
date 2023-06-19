897b71e1dcbd833bceec196df776374162f36e1c
AndrewVreeland <andrewvreeland@gmail.com>
2023-06-19 18:11:35 -0400

    added activity pages for user flow

---
+++ KnowledgeHUB/app/src/main/java/com/study/quizzler/activities/AnswersPage.java
@@ -0,0 +1,16 @@
+package com.study.quizzler.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.study.quizzler.R;
+
+public class AnswersPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answers_page);
+    }
+}
---
+++ KnowledgeHUB/app/src/main/java/com/study/quizzler/activities/SignUpPage.java
@@ -0,0 +1,16 @@
+package com.study.quizzler.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.study.quizzler.R;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
