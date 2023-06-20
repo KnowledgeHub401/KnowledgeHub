e582cef15d99d20ae2815ef48cde745699aed677
AndrewVreeland <andrewvreeland@gmail.com>
2023-06-20 11:54:23 -0400

    added intent to the nav_menu buttons

--- KnowledgeHUB/app/src/main/java/com/study/quizzler/listeners/NavigationItemSelectedListener.java
+++ KnowledgeHUB/app/src/main/java/com/study/quizzler/listeners/NavigationItemSelectedListener.java
@@ -1,10 +1,20 @@
 package com.study.quizzler.listeners;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.google.android.material.navigation.NavigationView;
+import com.study.quizzler.MainActivity;
 import com.study.quizzler.R;
+import com.study.quizzler.activities.AboutUs;
+import com.study.quizzler.activities.QuestionPage;
+import com.study.quizzler.activities.SettingsPage;
 
 public class NavigationItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,12 +34,20 @@ public class NavigationItemSelectedListener implements NavigationView.OnNavigati
 
         if (itemId == R.id.navMenuHomeButton) {
             showToast("Home Selected");
+            if (context instanceof MainActivity) {
+                closeNavigationDrawer();
+            } else {
+                openMainActivity();
+            }
         } else if (itemId == R.id.navMenuAboutUsButton) {
             showToast("About Us Selected");
+            openAboutUsPage();
         } else if (itemId == R.id.navMenuAnswersButton) {
             showToast("Questions Page Selected");
+            openQuestionsPage();
         } else if (itemId == R.id.navMenuSettingsButton) {
             showToast("Settings Selected");
+            openSettingsActivity();
         } else if (itemId == R.id.navMenuLogoutButton) {
             showToast("Logout Selected");
         } else if (itemId == R.id.navMenuRateUsButton) {
@@ -37,7 +55,36 @@ public class NavigationItemSelectedListener implements NavigationView.OnNavigati
         } else if (itemId == R.id.navMenuShareButton) {
             showToast("Share Selected");
         }
-
         return true;
+    }
+    private void closeNavigationDrawer() {
+        DrawerLayout drawerLayout = ((Activity) context).findViewById(R.id.drawer_layout);
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    private void openMainActivity() {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the activity stack
+        context.startActivity(intent);
+        ((Activity) context).finish(); // Finish the current activity
+    }
+    private void openSettingsActivity(){
+        Intent intent = new Intent(context, SettingsPage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the activity stack
+        context.startActivity(intent);
+        ((Activity) context).finish(); // Finish the current activity
+    }
+    private void openQuestionsPage(){
+        Intent intent = new Intent(context, QuestionPage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the activity stack
+        context.startActivity(intent);
+        ((Activity) context).finish(); // Finish the current activity
+    }
+    private void openAboutUsPage(){
+        Intent intent = new Intent(context, AboutUs.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the activity stack
+        context.startActivity(intent);
+        ((Activity) context).finish(); // Finish the current activity
     }
 }
