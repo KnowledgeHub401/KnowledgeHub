b55a052db87394531588b995f503fd560c022eca
Andrew Vreeland <122303931+AndrewVreeland@users.noreply.github.com>
2023-06-22 22:29:39 -0400

    fresh codegen models

--- app/src/main/java/com/study/quizzler/quizzlerApplication.java
+++ app/src/main/java/com/study/quizzler/quizzlerApplication.java
@@ -5,22 +5,17 @@ import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.AWSDataStorePlugin;
 
-public class quizzlerApplication extends Application {
-    public static final String TAG = "quizzlerapplication";
+public class QuizzlerApplication extends Application {
+    public static final String TAG = "lecture28demoapplication";
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-
         try {
-            Amplify.addPlugin(new AWSCognitoAuthPlugin());
             Amplify.addPlugin(new AWSApiPlugin());
-            Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.configure(getApplicationContext());
             Log.i(TAG, "Initialized Amplify");
         } catch (AmplifyException ae) {
@@ -28,5 +23,3 @@ public class quizzlerApplication extends Application {
         }
     }
 }
-
-
